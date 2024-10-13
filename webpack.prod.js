ad4a507c49e84fd53ca94098bcfc060fc91260f5
Jethro Guerrier <145587508+TheJeth@users.noreply.github.com>
2024-10-13 20:59:11 +0000

    Test 4

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -8,11 +8,8 @@ module.exports = merge(common, {
     publicPath: '/'
   },
   plugins: [
-    new Dotenv({
-      systemvars: true, // Load all system environment variables as well
-      safe: true, // Load '.env.example' to verify the '.env' variables are all set
-      silent: true,  // Hide any warnings
-      defaults: false // Don't load '.env.defaults'
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env)
     })
   ]
 });
